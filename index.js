c2f93991534466c398b1074a973ed4aca9ca8bbe
Jashan1604 <jashanpreet_su@ma.iitr.ac.in>
2025-11-27 22:05:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const b = 3;
 function add(a, b){
     return a+b;
 }
-console.log("hello 1");
+console.log("hello 1 Stashing done yo");
 console.log("hello new new");
 console.log("hello after new branch");
 console.log("hello in branch feature-b");
